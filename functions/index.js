679cc9f16535c5b9f9abcc2ddc1f9b2dfe459771
Anthony Mania <amj2544@tutanota.com>
2025-09-30 19:30:46 +0200

    Job Stripe

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,7 @@
-// import * as functions from "firebase-functions";
-// import { app } from "../tipbox-server/server.js";
+// functions/index.js
+import { createApp } from "./server.js";
+import admin from "firebase-admin";
+import * as functions from "firebase-functions/v2";
 
-
-// // Config Firebase
-// const stripeKey = functions.config().stripe.key;
-// const emailUser = functions.config().email.user;
-// const emailPass = functions.config().email.pass;
-
-// // Ici tu peux initialiser Stripe/Nodemailer avec ces valeurs
-// // par exemple dans server.js, au lieu de process.env.XXX, tu passes ces variables
-
-// // Déploiement de l'API Express comme fonction Firebase
-// export const api = functions.https.onRequest(app);
+export { apiV2 } from "./httpFunctions.js";
+export { newUserCreated } from "./triggers.js";
