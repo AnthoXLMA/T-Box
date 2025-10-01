574363c25b69bd0778e9f3c620f1e960209ff4a7
Anthony Mania <amj2544@tutanota.com>
2025-10-01 11:39:49 +0200

    Check 1

--- functions/triggers.js
+++
@@ -1,7 +0,0 @@
-// triggers.js
-import { onDocumentCreated } from "firebase-functions/v2/firestore";
-
-export const newUserCreated = onDocumentCreated("users/{uid}", (event) => {
-  const newUser = event.data;
-  console.log("Nouvel utilisateur créé :", newUser);
-});
